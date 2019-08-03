5462b0db889c5a7616388deda07ba2bb88432c06
Ondřej Sluka <ondras12345@gmail.com>
2019-08-03 21:52:29 +0200

    Add first working code

--- IR-vlc-control/IR_vlc_control.py
+++ IR-vlc-control/IR_vlc_control.py
@@ -1,25 +1,87 @@
-#Author: Ondrej Sluka
-#This code is meant to be run with Python 3
-#Features: 
+#! /usr/bin/env python3
+# Author: Ondrej Sluka
+# This code is made for Python3
+# Features:
+# * control VLC Media player via IR remote
+#
+# See:
+# * https://wiki.videolan.org/index.php?title=VLC_HTTP_requests
+# * https://github.com/z3t0/Arduino-IRremote
 
 import serial
 import signal
 import sys
+import requests
+import re
+import datetime
 
-def sigint_handler(signum, frame):   #ctrl+c
+COM_PORT_DEFAULT = 'COM11'
+
+PORT = 8080
+IP = 'localhost'
+URL = 'http://{}:{}/requests/status.xml'.format(IP, PORT)
+VLC_PASSWORD = 1234
+
+KEY_DICT = {
+    '5EA110EF': 'pl_play',
+    '5EA1906F': 'pl_pause',
+    # '5EA1906F': 'pl_stop',
+    '5EA150AF': 'pl_next',
+    '5EA1D02F': 'pl_previous',
+    }
+
+KEY_REPEAT_TIMEOUT = 2.5  # or 5 s
+
+
+def sigint_handler(signum, frame):  # ctrl+c
     ser.close()
     sys.exit(0)
 
 
-if __name__ == '__main__':
-    signal.signal(signal.SIGINT, sigint_handler) 
-    
-    ser = serial.Serial(
-                port='COM10',
-                baudrate=9600,
-                parity=serial.PARITY_NONE,
-                stopbits=serial.STOPBITS_ONE,
-                bytesize=serial.EIGHTBITS
-                )
-                
+def get_key_id(message):
+    try:
+        id = re.search('Decoded NEC: (.*) \(32 bits\)', message).group(1)
+    except AttributeError:
+        id = None
 
+    return id
+
+
+def match_key(id):
+    if id in KEY_DICT:
+        command = KEY_DICT[id]
+        print('Key matched: {}'.format(command))
+        requests.get(url=URL, params={'command': command}, auth=('', VLC_PASSWORD))
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, sigint_handler)
+    port = input('Enter serial port: ')
+    if port == '':
+        port = COM_PORT_DEFAULT
+        print('Defaulting to {}'.format(COM_PORT_DEFAULT))
+
+    ser = serial.Serial(port=port, baudrate=9600)
+
+    last_received_time = datetime.datetime.now()
+    last_received_key_id = None
+
+    while(True):
+        if ser.in_waiting > 0:
+            message = str(ser.read_until())  # wait for newline
+            print('Message got: {}'.format(message))
+
+            time_delta = datetime.datetime.now() - last_received_time
+
+            id = get_key_id(message)
+            print('ID got: {}'.format(id))
+
+            if id is not None:
+                if time_delta.total_seconds() > KEY_REPEAT_TIMEOUT or (not id == last_received_key_id and time_delta.total_seconds() > (KEY_REPEAT_TIMEOUT / 5)):
+                    match_key(id)
+
+                last_received_time = datetime.datetime.now()
+                last_received_key_id = id
